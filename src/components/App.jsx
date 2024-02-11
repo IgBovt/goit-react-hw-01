d3e71a94dd07ab76843e9b39f4cdb03a9f3c679e
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-02-11 14:45:30 +0200

    made 3rd task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import Profile from './Profile/Profile';
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import userData from '../data/userData.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transaction.json';
 
 const App = () => {
   return (
@@ -14,7 +16,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
-      {/* <TransactionHistory items={transactions} /> */}
+      <TransactionHistory items={transactions} />
     </>
   );
 };
